ef71d8224caa8ab081aca23e1500c0320d0fb893
jkkang <kjk515@naver.com>
2023-06-09 22:26:06 +0900

    Init

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        assertEquals("Test", "Test");
+    }
+}
