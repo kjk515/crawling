a469454ea26406e3f62409bb418fefb730c3c475
jkkang <kjk515@naver.com>
2023-06-11 09:10:41 +0900

    timeout 로직추가

--- src/main/java/com/jin/crawling/infrastructure/CrawlingClientImpl.java
+++ src/main/java/com/jin/crawling/infrastructure/CrawlingClientImpl.java
@@ -1,24 +1,60 @@
 package com.jin.crawling.infrastructure;
 
 import com.jin.crawling.config.CrawlingProperties;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.springframework.stereotype.Repository;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
-@Repository
+@Component
 public class CrawlingClientImpl implements CrawlingClient {
 
     private final int timeout;
+    private final int maxRetry;
+    private final ConcurrentMap<String, Integer> retryCount = new ConcurrentHashMap<>();
+    private static final int INIT_WAITING_TIME = 100;
+
 
     public CrawlingClientImpl(CrawlingProperties crawlingProperties) {
         this.timeout = crawlingProperties.timeout();
+        this.maxRetry = crawlingProperties.maxRetry();
     }
 
     @Override
-    public String getHtml(String url) throws IOException {
-        Connection conn = Jsoup.connect(url).timeout(timeout);
-        return conn.get().toString();
+    public String getHtml(String url) {
+        Document document = null;
+        try {
+            document = Jsoup.connect(url).timeout(timeout).get();
+        } catch (SocketTimeoutException e) {
+            this.handleTimeout(url, e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return document.toString();
+    }
+
+    private void handleTimeout(String url, SocketTimeoutException timeoutException) {
+        retryCount.putIfAbsent(url, 0);
+        int count = retryCount.get(url);
+
+        if (count < maxRetry) {
+            retryCount.put(url, count + 1);
+            try {
+                Thread.sleep(this.randomBackOffTime(count));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            this.getHtml(url);
+        } else {
+            throw new RuntimeException(timeoutException);
+        }
+    }
+
+    private long randomBackOffTime(int count) {
+        return (long) (INIT_WAITING_TIME * Math.pow(2, count) + Math.random() * 500);
     }
 }
--- src/test/java/com/jin/crawling/infrastructure/CrawlingClientTest.java
+++ src/test/java/com/jin/crawling/infrastructure/CrawlingClientTest.java
@@ -1,12 +1,15 @@
 package com.jin.crawling.infrastructure;
 
+import com.jin.crawling.config.CrawlingProperties;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -15,6 +18,9 @@ public class CrawlingClientTest {
     @InjectMocks
     private CrawlingClientImpl crawlingClient;
 
+    @Mock
+    private CrawlingProperties crawlingProperties;
+
     @Test
     public void getTest() throws IOException {
         // given
@@ -22,5 +28,12 @@ public class CrawlingClientTest {
 
         String crawlingText = crawlingClient.getHtml("https://www.kia.com");
         System.out.println(crawlingText);
+    }
+
+    @Test
+    public void retryTimeTest() {
+        assertEquals(100 * Math.pow(2, 0), 100);
+        assertEquals(100 * Math.pow(2, 1), 200);
+        assertEquals(100 * Math.pow(2, 2), 400);
     }
 }
